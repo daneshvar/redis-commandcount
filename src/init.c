90a64af7d4feb9a177090b7867932ab79acab05c
Hussein Daneshvar <daneshvar.hu@gmail.com>
2019-10-13 03:16:58 +0330

    fix project name fix command name

--- src/init.c
+++ src/init.c
@@ -1,11 +1,11 @@
 #include "cmd.h"
 
 int RedisModule_OnLoad(RedisModuleCtx* ctx, RedisModuleString** argv, int argc) {
-  if (RedisModule_Init(ctx, COUNTCMD_MOD_NAME, COUNTCMD_MOD_VER, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
+  if (RedisModule_Init(ctx, CMDCOUNT_MOD_NAME, CMDCOUNT_MOD_VER, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 
-  if (RedisModule_CreateCommand(ctx, "‫‪commandcount‬‬", CommandCount, "readonly", 0, 0, 0) == REDISMODULE_ERR) {
+  if (RedisModule_CreateCommand(ctx, "commandcount", CommandCount, "readonly", 0, 0, 0) == REDISMODULE_ERR) {
     return REDISMODULE_ERR;
   }
 
